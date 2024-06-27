c8229fdb12ee34a6b39bfb42446c28d0c19e5a74
Rahul4533 <106775929+Rahul4533@users.noreply.github.com>
2024-06-27 13:27:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'
 import connectDB from "./Config/Db.js";
 import router from "./Router/registerForm.js";
+import cors from 'cors'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -14,15 +15,17 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 //middle ware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors({origin: "http://localhost:8000"}));
+
 app.use('/',router);
  
    
  
-app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // Handle all other routes by serving the 'index.html' file
 app.get('*', (req, res) => {
-    const index = join(__dirname, './client/dist/index.html');
+    const index = join(__dirname, './client/build/index.html');
     res.sendFile(index);
 });
 
@@ -31,5 +34,5 @@ app.listen(port,(err)=>{
     if(err){
         console.log(err);
     }
-    console.log(`Server is running on Port ${process.env.PORT}`)
+    console.log(`Server is running on Port ${port}`)
 })  
