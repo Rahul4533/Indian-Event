2ce15bc2b01cbc8f57bea07138fbea2a12bbdb39
Rahul <Rahulsingh4533@gmail.com>
2024-07-03 16:22:06 +0530

    Competed Image

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -1,161 +1,243 @@
+const BollyWood = [
+  {
+    id: 1,
+    name: "Sonu Nigam",
+    image:
+      "https://lastfm.freetls.fastly.net/i/u/avatar170s/24931fb5cf2142498630446de2c43e21",
+  },
+  {
+    id: 2,
+    name: "Neha Kakkar",
+    image:
+      "https://lastfm.freetls.fastly.net/i/u/avatar170s/112a7dc1d76928a4289d02c12f181d85",
+  },
+  {
+    id: 3,
+    name: "Udit Narayan",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNDh5gZ2MtTI-FJDl-aEjNPKjnhWgMkdq19cKDnyvOc01ik-k2sKklRML4nQ&s",
+  },
+  {
+    id: 4,
+    name: "Altaf Raja",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmop_RsJNmz6uHCQz45Xn3bG4dia64C1m4CQ&s",
+  },
+  {
+    id: 5,
+    name: "Badshah",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRItEixREdkd0VXvJJHbkFgIWsr-24olPU4mg&s",
+  },
+  {
+    id:6,
+    name: "Hani Singh",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vrSWFJE-0CKBXetjpSr5jH3g9npwLCd6LQ&s",
+  },
+  {
+    id: 7,
+    name: "Pawan Deep Rajan",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqWT3ManUz9c6sv6Fp8DbW89_hrbLMYSiqHA&s",
+  },
+  {
+    id: 8,
+    name: "Arman Malik",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnGyaaLXLMGSWRuzk07CbvR-cUQC05rMuD2g&s",
+  },
+  {
+    id: 9,
+    name: "Sunidhi Chuhan",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSekNOZGhabMBcRaz-PtEQAQ62grUk7N_6vMQ&s",
+  },
+  {
+    id: 10,
+    name: "Arjit Singh",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEaO9oNwP0hT6rCxrP1_xtB2Q_ORBTsB3cmw&s",
+  },
+  {
+    id: 11,
+    name: "Kailash Kher",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS05uxkXRUAqYhMGRuHCMj1MLSYhEzlWdmz6w&s",
+  },
+  {
+    id: 14,
+    name: "SalMan Khan",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKPUbBlc9-ogkiC_rR5s7JEAyED54z_BnIVw&s",
+  },
+  {
+    id: 15,
+    name: "Sharukh Khan",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdJkrAlVUKfQ4bv-7jy3ybpNQGS_D75qu1fw&s",
+  },
+  {
+    id: 16,
+    name: "Madhuri Dikshit",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRW8X5YvdOMZP-WaEBYgkAdX-gmoj1CLPPoQ&s",
+  },
+  {
+    id: 17,
+    name: "Sonakshi Sinha ",
+    image:
+      "https://assets.vogue.in/photos/66194ec7c1176ee44f675a8b/3:4/w_2560%2Cc_limit/436732623_18431809969010396_8730273379753962545_n.jpg",
+  },
+  {
+    id: 18,
+    name: "Sunny Leone",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKk7nN_reDcI2jVfSUyfAMhFbspDBfKPSQJg&s",
+  },
+  {
+    id: 19,
+    name: "Kumar Sanu",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR4MXo-Nn1EgoJJbNs2rlVasmBSu6-Ch6MkA&s",
+  },
+  {
+    id: 20,
+    name: "Malika Arora",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbzpVUgbXX5jBrlpQsOvKalzErpCTi1GEDVw&s",
+  },
+  {
+    id: 21,
+    name: "Katrina Kaif",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMUT9WFU6KEwu69l5wTSGGHT4AagDcHnPKdA&s",
+  },
+  {
+    id: 22,
+    name: "Shardha Kappor",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr3i1deW7P3qgEQ4QCU84CdNM2NPU-wzsuJQ&s",
+  },
+  {
+    id: 23,
+    name: "Allia Bhatt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGsGsFrtqiBZTXDpiufnX1I2JLnbeSYxzLJw&s",
+  },
+  {
+    id: 24,
+    name: "Priyanka chopra",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFVhdozZGhEnAIB5kN-QQ9ZgznKEgMjAw3Bw&s",
+  },
+  {
+    id: 25,
+    name: "Sonam Kapoor",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX2OMb1HvNosx42rxK9AJrT5bcgLPZrUuagQ&s",
+  },
+];
+
+const Bhojpuri = [
+  {
+    id: 1,
+    name: "Pawan Singh",
+    image:
+      "https://w0.peakpx.com/wallpaper/660/192/HD-wallpaper-pawan-singh-bhojpuri-pawan-singh-singer.jpg",
+  },
+  {
+    id: 2,
+    name: "Kheshari Lal",
+    image: "https://i.scdn.co/image/ab6761610000e5eb034aea83b2e079e11db4eb53",
+  },
+  {
+    id: 3,
+    name: "Golu Raja",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsr4_WPqq25ICwplHwZJtaS2xoW2yVtLBUhw&s",
+  },
+  {
+    id: 4,
+    name: "Tuntun Yadav",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkY1HA1XRpVzRIonIB6GwT2OhFQNtK1bsjvA&s",
+  },
+  {
+    id: 5,
+    name: "Jyoti Mahi",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUWM2OxtUxkmix97g3EEF_KBgFXVwtN6TlfA&s",
+  },
+  {
+    id:6,
+    name: "Chandan Yadav",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0xBw40E6STRa9657LTJZI1-nBrUEeMO3U1A&s",
+  },
+  {
+    id: 7,
+    name: "Promod Premi yadav",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrwqc6Jl3FHfG2oc3SJnMqvf1vNXbQ_6_IVg&s",
+  },
+  {
+    id: 8,
+    name: "Gunjan Singh",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc_r0S11n9bQyB2UwL3qRIKV7BBGaMZ2XsZQ&s",
+  },
+  {
+    id: 9,
+    name: "Chhotu Chhaliya",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc4OS3poYhpA0fUdm4mPWhTDdO000qL9b6uQ&s",
+  },
+  {
+    id: 10,
+    name: "Nilkaml Singh",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7fxfTCwbLHepS1nSiNGfBbd3lCLMzttFwjw&s",
+  },
+  {
+    id: 11,
+    name: "Samar Singh",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4k-rVaqhrqppoNU2kxH8wThufrq1DcMXURw&s",
+  },
+  {
+    id: 12,
+    name: "Shivani Singh",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnHCDlChWapuW-UWBgbHp59BK9J1Hruy3Cjw&s",
+  },
+  {
+    id: 13,
+    name: "Shilpi Raj",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGtTZGHmpvJb8VQZPQmBlIYJsiifvfpuckkA&s",
+  },
+  {
+    id: 14,
+    name: "Antra Singh Priyanka",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLlq1TWNcw1lXRxPQGDSNm-m6tSDQ6eKzF0A&s",
+  },
+  {
+    id: 15,
+    name: "Anupam Yadav",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHQYDFFIbXrMWyvgFUqBCZ9l8q3NJNgZoikA&s",
+  },
+  {
+    id: 16,
+    name: "Akshra Singh",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbjImfFC89QyntGkYgf1e666T9Wvx0upWHEQ&s",
+  },
+];
+
+const Bengali = [
+  {
+    id: 1,
+  },
+];
+
+const Punjabi = [
+  {
+    id: 1,
+    name: "Diljit Dosanjh",
+    image: "",
+  },
+];
 
 
 
-const BollyWood=[
-    {
-        id:1,
-        name:"Sonu Nigam",
-        image:"https://lastfm.freetls.fastly.net/i/u/avatar170s/24931fb5cf2142498630446de2c43e21"
-
-    },{
-        id:2,
-        name:"Neha Kakkar",
-        image:"https://lastfm.freetls.fastly.net/i/u/avatar170s/112a7dc1d76928a4289d02c12f181d85"
-
-    },
-    {
-        id:3,
-        name:"Udit Narayan",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNDh5gZ2MtTI-FJDl-aEjNPKjnhWgMkdq19cKDnyvOc01ik-k2sKklRML4nQ&s"
-    },
-    {
-        id:4,
-        name:"Altaf Raja",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmop_RsJNmz6uHCQz45Xn3bG4dia64C1m4CQ&s"
-    },
-    {
-        id:5,
-        name:"Badshah",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRItEixREdkd0VXvJJHbkFgIWsr-24olPU4mg&s"
-    },
-    {
-        id:6,
-        name:"Hani Singh",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vrSWFJE-0CKBXetjpSr5jH3g9npwLCd6LQ&s"
-    },
-    {
-        id:7,
-        name:"Pawan Deep Rajan",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqWT3ManUz9c6sv6Fp8DbW89_hrbLMYSiqHA&s"
-    },
-    {
-        id:8,
-        name:"Arman Malik",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnGyaaLXLMGSWRuzk07CbvR-cUQC05rMuD2g&s"
-    },
-    {
-        id:9,
-        name:"Sunidhi Chuhan",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSekNOZGhabMBcRaz-PtEQAQ62grUk7N_6vMQ&s"
-    },
-    {
-        id:10,
-        name:"Arjit Singh",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEaO9oNwP0hT6rCxrP1_xtB2Q_ORBTsB3cmw&s"
-    }
-    ,
-    {
-        id:11,
-        name:"Kailash Kher",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS05uxkXRUAqYhMGRuHCMj1MLSYhEzlWdmz6w&s"
-    },
-    {
-        id:14,
-        name:"SalMan Khan",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKPUbBlc9-ogkiC_rR5s7JEAyED54z_BnIVw&s"
-    },
-    {
-        id:15,
-        name:"Sharukh Khan",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdJkrAlVUKfQ4bv-7jy3ybpNQGS_D75qu1fw&s"
-    },
-    {
-        id:16,
-        name:"Madhuri Dikshit",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRW8X5YvdOMZP-WaEBYgkAdX-gmoj1CLPPoQ&s"
-    },
-    {
-        id:17,
-        name:"Sonakshi Sinha ",
-        image:"https://assets.vogue.in/photos/66194ec7c1176ee44f675a8b/3:4/w_2560%2Cc_limit/436732623_18431809969010396_8730273379753962545_n.jpg"
-    },
-    {
-        id:18,
-        name:"Sunny Leone",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKk7nN_reDcI2jVfSUyfAMhFbspDBfKPSQJg&s"
-    },
-    {
-        id:19,
-        name:"Kumar Sanu",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR4MXo-Nn1EgoJJbNs2rlVasmBSu6-Ch6MkA&s"
-    },
-    {
-        id:20,
-        name:"Malika Arora",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbzpVUgbXX5jBrlpQsOvKalzErpCTi1GEDVw&s"
-    },
-    {
-        id:21,
-        name:"Katrina Kaif",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMUT9WFU6KEwu69l5wTSGGHT4AagDcHnPKdA&s"
-    },
-    {
-        id:22,
-        name:"Shardha Kappor",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr3i1deW7P3qgEQ4QCU84CdNM2NPU-wzsuJQ&s"
-    },
-    {
-        id:23,
-        name:"Allia Bhatt",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGsGsFrtqiBZTXDpiufnX1I2JLnbeSYxzLJw&s"
-    },
-    {
-        id:24,
-        name:"Priyanka chopra",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFVhdozZGhEnAIB5kN-QQ9ZgznKEgMjAw3Bw&s"
-    },
-    {
-        id:25,
-        name:"Sonam Kapoor",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX2OMb1HvNosx42rxK9AJrT5bcgLPZrUuagQ&s"
-    }
-
-]
-
-
-const Bhojpuri=[
-    {
-        id:1,
-        name:"Pawan Singh",
-        image:"https://w0.peakpx.com/wallpaper/660/192/HD-wallpaper-pawan-singh-bhojpuri-pawan-singh-singer.jpg"
-    },
-    {
-        id:2,
-        name:"Kheshari Lal",
-        image:"https://i.scdn.co/image/ab6761610000e5eb034aea83b2e079e11db4eb53"
-    }
-]
-
-const Bengali=[
-    {
-         id:1
-    }
-]
-
-
-const Punjabi=[
-    {
-        id:1,
-        name:"Diljit Dosanjh",
-        image:""
-    }
-]
-
-const videos=[
-     {
-        id:1,
-        video:"https://youtu.be/lMIS0rDvmqI?si=QFXVrClEPrSqoRYe"
-     }
-]
-
-module.exports  = {Bengali,Bhojpuri,BollyWood,Punjabi,videos};
+module.exports = { Bengali, Bhojpuri, BollyWood, Punjabi};
--- client/src/components/Actor.jsx
+++ client/src/components/Actor.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './actor.css';
+
 const Actor = React.memo(({ data }) => {
-  // This will help you see when the component re-renders
-   console.log(data);
-  
+  // State to manage the popup
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  // Function to open the popup
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  // Function to close the popup
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <>
-  <div className="card-container" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
-    {data?.map((item, index) => (
-      <div key={index} className="card-item">
-        <div>
-          <img className="card-img-top" src={item.image} alt="Card image cap" />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">{item.name}</h5>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
+      <div className="card-container">
+        {data?.map((item, index) => (
+          <div key={index} className="card-item">
+            <div className="card-img-container">
+              <img className="card-img-top" src={item.image} alt={`${item.name} image`} />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <button className="btn btn-enquiry" onClick={openPopup}>Enquiry</button>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-</>
 
+      {isPopupOpen && (
+        <>
+        <div className="popup-overlay active" onClick={closePopup}></div>
+        <div className="popup active">
+          <button className="popup-close" onClick={closePopup}>&times;</button>
+          <h2>Enquiry Form</h2>
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" required />
+            </label>
+            <label>
+              Email:
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Message:
+              <textarea name="message" required></textarea>
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </>
+    )}
+  </>
   );
 }, (prevProps, nextProps) => {
   // Custom comparison function
