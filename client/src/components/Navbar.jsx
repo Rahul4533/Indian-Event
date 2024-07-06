ca0c8fe4c44237c908426497bfdb135f4c1f3e4f
Rahul <Rahulsingh4533@gmail.com>
2024-07-06 23:16:14 +0530

    adding navbar with style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,16 +2,16 @@
 
 import React, { useState } from 'react';
 import './navbar.css'; // Import your CSS file for styling
-import {Bhojpuri,Bengali,Punjabi, BollyWood} from '../../src/Data/data.js';
+import { Bhojpuri, Bengali, Punjabi, BollyWood } from '../../src/Data/data.js';
 import Actor from './Actor.jsx';
 import Drawer from './Drawer/Drawer.jsx';
-const Navbar = () => {
-  
+import img from '../../src/assects/image/Indian-event.png';
 
+const Navbar = () => {
   const [selectedOption, setSelectedOption] = useState('');
   const [actor, setActor] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  
   const handleChange = (event) => {
     const selectedValue = event.target.value;
     setSelectedOption(selectedValue);
@@ -37,30 +37,30 @@ const Navbar = () => {
     }
   };
 
-  const [isOpen, setIsOpen] = useState(false);
-
   const toggleDrawer = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <>
-    <div className="m-2 navbar">
-      <div className="dropdown">
-        <select className="custom-select" value={selectedOption} onChange={handleChange}>
-          <option value="" disabled hidden>Book Now</option>
-          <option value="Bollywood">Bollywood</option>
-          <option value="Bhojpuri">Bhojpuri</option>
-          <option value="Bengali">Bengali</option>
-          <option value="Punjabi">Punjabi</option>
-        </select>
-        
+      <div className="navbar">
+        <div className="navbar-logo">
+          <img src={img} alt="logo" />
+        </div>
+        <div className="navbar-dropdown">
+          <select className="custom-select" value={selectedOption} onChange={handleChange}>
+            <option value="" disabled hidden>Book Now</option>
+            <option value="Bollywood">Bollywood</option>
+            <option value="Bhojpuri">Bhojpuri</option>
+            <option value="Bengali">Bengali</option>
+            <option value="Punjabi">Punjabi</option>
+          </select>
+        </div>
+        <button className="drawer-btn" onClick={toggleDrawer}><i className="fa-solid fa-bars"></i></button>
       </div>
-      <button className="drawer-btn" onClick={toggleDrawer}><i className="fa-solid fa-bars"></i></button>
-     
-    </div>
-    <Actor data={actor} />
-    <Drawer isOpen={isOpen} toggleDrawer={toggleDrawer} />
-  </>
+      <Actor data={actor} />
+      <Drawer isOpen={isOpen} toggleDrawer={toggleDrawer} />
+    </>
   );
 };
 
